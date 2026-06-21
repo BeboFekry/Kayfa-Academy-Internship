3ab39fdf7f5089914e73864a233b0cae1374e9c1
Abdallah Fekry <116501243+BeboFekry@users.noreply.github.com>
2026-06-22 02:01:54 +0300

    Update app.py

--- Task_3_Sales_AI_Agent/app.py
+++ Task_3_Sales_AI_Agent/app.py
@@ -1,6 +1,8 @@
 import streamlit as st
 
-st.logo("/media/kayfaio_logo2.png")
+current_dir = os.path.dirname(os.path.abspath(__file__))
+logo_path = os.path.join(current_dir, "media", "kayfaio_logo2.png")
+st.logo(logo_path)
 st.set_page_config(page_icon='media/icon.png', page_title='Kayfa Chatbot')
 
 home = st.Page(r"home.py", title="Home", icon=":material/home:", default=True)
